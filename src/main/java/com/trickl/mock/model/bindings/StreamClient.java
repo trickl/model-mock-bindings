9e29cbc6c14320b56805a9de527a8152815ec83a
Tim Gee <writetotimgee@gmail.com>
2020-11-21 12:49:42 +0000

    Use cached mock streams for each instrument

--- src/main/java/com/trickl/mock/model/bindings/StreamClient.java
+++ src/main/java/com/trickl/mock/model/bindings/StreamClient.java
@@ -1,11 +1,22 @@
 package com.trickl.mock.model.bindings;
 
+import com.trickl.flux.routing.MapRouter;
 import com.trickl.model.pricing.primitives.OrderBook;
+import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 
 public class StreamClient {
-  public Flux<OrderBook> subscribeQuotes(String instrumentCode) {
+  private MapRouter<OrderBook> mapRouter = MapRouter
+      .<OrderBook>builder()
+      .fluxCreator(this::generateQuotes)
+      .build();
+
+  private Flux<OrderBook> generateQuotes(Publisher<OrderBook> source, String instrumentCode) {
     MockInstrumentImpl instrument = MockInstruments.get(instrumentCode);
     return instrument.getOrderBookStream();
+  }
+
+  public Flux<OrderBook>  getQuotes(String instrumentCode) {
+    return mapRouter.route(Flux.empty(), instrumentCode);
   }
 }
